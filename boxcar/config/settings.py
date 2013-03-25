ef58c8a4fd1086cf1b38cc1ab9554e3e3ebff405
tpalko <tpalko@redbluevoice.com>
2013-03-25 19:25:47 -0400

    some modifications to settings.py to make it work with the config folder

--- boxcar/config/settings.py
+++ boxcar/config/settings.py
@@ -102,10 +102,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'boxcar.urls'
+ROOT_URLCONF = 'config.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'boxcar.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
